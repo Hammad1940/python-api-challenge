b17b7f6b1fdee4bfd67c3c38fe6e256e93c6fbe1
Hammad1940 <115465543+Hammad1940@users.noreply.github.com>
2022-12-05 03:01:09 +0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "30b1cef6a3001346458297f727f46417"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d8db679a8c264246be21ab88d9362f4d"
